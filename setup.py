f789908ca6389390a1f93a9881da8fe52a4ed17f
I AM MIKEY <86167689+MoeZilla@users.noreply.github.com>
2021-11-18 18:38:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,32 @@
-import setuptools
+from setuptools import setup
 
-with open('requirements.txt') as f:
-    dependencies = [l.strip() for l in f]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name='sylviorus',
-    version='0.1',
-    description='Sylviorus Wrapper',
+
+setup(
+    name="slyWrappy",
+    version="0.4",
+    description="sylviorus API wrapper",
+    py_modules=["dependencies"],
+    package_dir={'': 'sylviorus'},
+    install_requires=["requests", "typing"],
+    extras_require={
+                      "dev":[
+                          "pytest",
+                      ],},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-    author='NkSamaX',
-    author_email='-',
-    url='https://github.com/NksamaX/Syl-Py.git',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Typing :: Typed'
-    ],
-    install_requires=dependencies,
-    python_requires='>=3.6'
+    url="https://github.com/NksamaX/Syl-Py",
+    author="Nksamax"
+
 )
